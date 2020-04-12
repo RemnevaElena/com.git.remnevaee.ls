d81a05524cd9a6b11c90c5349572534bfacd53ef
RemnevaElena <remneva.ee@edu.spbstu.ru>
2020-04-12 12:26:44 +0300

    /

--- src/test/java/com/git/remnevaee/ls/ls.java
+++ src/test/java/com/git/remnevaee/ls/ls.java
@@ -1,9 +1,30 @@
 package com.git.remnevaee.ls;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 public class ls {
-    public static void main(String[] args) {
+    public static void main(String[] args) {}
 
-        System.out.println("pendir. i nad etim ja rewela 3 h");
+    protected static final class ProgramFile {
+        private final File file;
 
+        protected ProgramFile(File file) {
+            this.file = file;
+        }
+    }
+    private long Size() {
+        return file.length();
+    }
+    private String Name () {
+        return file.getName();
+    }
+    protected String LastModificate() {
+        final long timeModified = file.lastModified();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        return sdf.format(new Date(timeModified));
     }
 }
