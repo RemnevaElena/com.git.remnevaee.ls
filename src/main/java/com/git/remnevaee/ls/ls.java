2e5c070e1a54a98d5b7d8493509050fb92d3ceca
RemnevaElena <remneva.ee@edu.spbstu.ru>
2020-04-12 20:23:46 +0300

    /

--- src/main/java/com/git/remnevaee/ls/ls.java
+++ src/main/java/com/git/remnevaee/ls/ls.java
@@ -1,4 +1,68 @@
 package com.git.remnevaee.ls;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class ls {
+    public static void main(String[] args) {}
+
+    protected static final class ProgramFile {
+        private final File file;
+
+        protected ProgramFile(File file) {
+            this.file = file;
+        }
+
+        private long Size() {
+            return file.length();
+        }
+
+        private String Name() {
+            return file.getName();
+        }
+
+        protected String LastModificate() {
+            final long timeModified = file.lastModified();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+            return sdf.format(new Date(timeModified));
+        }
+
+        protected String СlearSize() {
+            long clearSize = Size();
+            String[] unit = new String[]{"B", "Kb", "Mb", "Gb"};
+            int count = 0;
+            while (clearSize >= 1024) {
+                clearSize /= 1024;
+                count++;
+            }
+            return clearSize + " " + unit[count];
+        }
+
+        protected String FilePermissions() {
+            StringBuffer sb = new StringBuffer();
+            if (file.canWrite()) {
+                sb.append("Write ");
+            } else {
+                sb.append("NoWrite ");
+            }
+            if (file.canRead()) {
+                sb.append("Read ");
+            } else {
+                sb.append("NoRead ");
+            }
+            if (file.canExecute()) {
+                sb.append("Execute ");
+            } else {
+                sb.append("NoWriteExecute");
+            }
+            return sb.toString();
+        }
+
+    }
 }
--- src/test/java/com/git/remnevaee/ls/ls.java
+++ src/test/java/com/git/remnevaee/ls/ls.java
@@ -7,24 +7,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 public class ls {
-    public static void main(String[] args) {}
 
-    protected static final class ProgramFile {
-        private final File file;
 
-        protected ProgramFile(File file) {
-            this.file = file;
-        }
-    }
-    private long Size() {
-        return file.length();
-    }
-    private String Name () {
-        return file.getName();
-    }
-    protected String LastModificate() {
-        final long timeModified = file.lastModified();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-        return sdf.format(new Date(timeModified));
-    },
 }
